cdbcf96110b434576847cf7b2d5f36bfe9f3f4d4
lisiCAO <caolisi@hotmail.com>
2023-09-01 22:38:08 -0400

    responsive nav&login

--- project/login&nav_responsive/scripts.js
+++ project/login&nav_responsive/scripts.js
@@ -5,20 +5,43 @@
 //     Contact: lisi.cao@johnabbottcollege.net
 //     Copyright: 2023
 
+// Wait for DOM content to load before executing script
+
+
 document.addEventListener('DOMContentLoaded', function() {
     // get parameters
+    var loginLink = document.querySelector('.login-link');
     var loginBtn = document.querySelector('.login-btn');
     var loginModal = document.querySelector('.login-modal');
     var closeBtn = document.querySelector('.close-btn');
-    
+    var offcanvas = document.querySelector('.offcanvas');
+    var hamburger = document.querySelector('.hamburger-icon');
+
+    loginLink.addEventListener('click', showLoginModal);
+    loginBtn.addEventListener('click', showLoginModal);
     // click to open page
-    loginBtn.addEventListener('click', function() {
+    function showLoginModal() {
         loginModal.style.display = 'block';
-    });
+    }
+
+
+
     
     // click to close page
     closeBtn.addEventListener('click', function() {
         loginModal.style.display = 'none';
+    });
+
+       // Toggle offcanvas
+       hamburger.addEventListener('click', function() {
+        offcanvas.classList.toggle('active');
+    });
+
+    // Close offcanvas when clicking outside of it
+    document.addEventListener('click', function(event) {
+        if (!offcanvas.contains(event.target) && !hamburger.contains(event.target)) {
+            offcanvas.classList.remove('active');
+        }
     });
 });
 
